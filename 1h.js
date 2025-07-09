dbe15bed053a0fb6a07a137b27e8e685cf5b93eb
textlogic <textlogic@gmail.com>
2025-07-09 13:02:30 -0500

    Add files via upload

---
+++ 1h.js
@@ -0,0 +1,38 @@
+PDF_HILIGHTING_NAV = {
+  "Dates": [
+    "10/7/13",
+    "9/3/2013",
+    "9/28/13"
+  ],
+  "Names": [
+    "Krista Switzer",
+    "Ellen Goodwater",
+  ],
+  "Medications": [
+    "Tylenol",
+    "Zithromax"
+  ],
+  "Vitals": [
+    "BP: 90/68",
+    "P: 78",
+    "T: 100.6 (tympanic)",
+    "R: 18",
+    "WT: 30lbs",
+    "HT: 34 inches",
+    "BMI: 18.2"
+  ],
+  "Tests": [
+    "Rapid strep in office"
+  ],
+  "Diagnoses": [
+    "Strep Pharyngitis",
+    "Upper respiratory infection",
+    "Eustachian tube dysfunction",
+    "Serous otitis",
+    "Otitis media",
+    "Pneumonia"
+  ],
+  "Medical Devices": []
+};
+
+PURL = '1.pdf'; 
---
+++ epmr_category_colors.js
@@ -0,0 +1,9 @@
+    const CATEGORY = {
+      "Names": "#FFFF00",
+      "Dates": "#ADD8E6",
+      "Medications": "#00FF00",
+      "Vitals": "#FFA500",
+      "Diagnoses": "#FF99F6",
+      "Tests": "#FFB6C1",
+      "Devices": "#00FFFF"
+    };
